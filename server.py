a6136467211ce74c4552fa29d0c0c1e506da51b5
Noam266 <noammaimon26@gmail.com>
2024-11-15 08:45:27 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 # Setup server details
 HOST = '127.0.0.1'  # Localhost, change to '' to listen on all network interfaces
-PORT = 65432        # Port to listen on (use any free port above 1024)
+PORT = 1246        # Port to listen on (use any free port above 1024)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: 
   server_socket.bind((HOST, PORT))  # Bind the socket to the address and port
   server_socket.listen()            # Enable the server to accept connections
